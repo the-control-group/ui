bd777ee7f03fb1e711705bbd17b121807d8b5a91
Adam Rich <adam.rich@thecontrolgroup.com>
2018-06-28 11:01:38 -0700

    cleanwebpack plugin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './styles/base.less';
 import './styles/variables.less';
 
-export { default as Grid } from './elements/Grid/Grid';
 export { default as Button } from './elements/Button/Button';
