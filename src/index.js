ff10f3230e07c761b4bb8de6b9090631d8a01353
Adam Rich <adam.rich@thecontrolgroup.com>
2018-07-03 15:14:47 -0700

    grid + card

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles/base.less';
 import './styles/variables.less';
 
+export { default as Grid } from './elements/Grid/Grid';
 export { default as Section } from './elements/Section/Section';
 export { default as Button } from './elements/Button/Button';
--- storybook/stories/Section.doc.js
+++ storybook/stories/Section.doc.js
@@ -5,21 +5,73 @@ import PropsTable from '../components/PropsTable';
 
 import { Section } from '../../src';
 
+const cardSubsectionExample = `<Section card>
+	<Section subsection>Card subsection #1</Section>
+	<Section subsection>Card subsection #2</Section>
+</Section>`;
+
 storiesOf('Elements', module)
 	.add('Section', () => (
 		<div>
 			<div className="story-heading">
 				<h1>Section</h1>
-				<p>Container element</p>
+				<p>Container styles and properties</p>
 			</div>
 
 			<div className="story-component-group">
 				<h2>Generic:</h2>
-				<p>Use the standard <pre>Section</pre> with no other props as a replacement for <pre>div</pre> in order to expose common UI properties to your containers</p>
+				<p>Use the standard <pre>Section</pre> with no other props as a replacement for <pre>div</pre> in order to expose common UI properties to your containers.</p>
 
 				<div className="component">
-					<Section>generic container example</Section>
-					<code>{'<Section>generic container</Section>'}</code>
+					<Section>This is just some text wrapped in a div</Section>
+					<code>{'<Section>Generic container stuff</Section>'}</code>
+				</div>
+			</div>
+
+			<div className="story-component-group">
+				<h2>Items:</h2>
+				<p>item stuff</p>
+
+				<div className="component">
+					<div>
+						<Section>
+							<Section item>Section item #1</Section>
+							<Section item>Section item #2</Section>
+							<Section item>Section item #3</Section>
+						</Section>
+					</div>
+
+					<code>{'<Section item>This is a section item</Section>'}</code>
+				</div>
+			</div>
+
+			<div className="story-component-group">
+				<h2>Card:</h2>
+				<p>Use the <pre>card</pre> flag to render a card (an enclosed, three-dimensional container).</p>
+
+				<div className="component">
+					<div className="card-example">
+						<Section card>Card eample</Section>
+					</div>
+
+					<code>{'<Section card>This is a standard card</Section>'}</code>
+				</div>
+			</div>
+
+			<div className="story-component-group">
+				<h2>Card Subsections:</h2>
+				<p>card subsection stuff</p>
+
+				<div className="component">
+					<div className="card-example">
+						<Section card>
+							<Section subsection>Card subsection #1</Section>
+							<Section subsection>Card subsection #2</Section>
+							<Section subsection>Card subsection #3</Section>
+						</Section>
+					</div>
+
+					<code className="multi">{cardSubsectionExample}</code>
 				</div>
 			</div>
 
