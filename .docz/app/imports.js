d69142b8c5cf6c032274b8abd713e320b83709b1
Adam Rich <adam.rich@thecontrolgroup.com>
2018-07-05 16:42:51 -0700

    working with DOCZ

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,4 +1,6 @@
 export const imports = {
   'docs/Button.doc.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "docs-button-doc" */ 'docs/Button.doc.mdx'),
+  'docs/Grid.doc.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-grid-doc" */ 'docs/Grid.doc.mdx'),
 }
--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,25 @@
 import { css } from 'docz-plugin-css';
 
 export default {
+	themeConfig: {
+		styles: {
+			container: {
+				width: 1100
+			},
+			pre: {
+				padding: ['0.5em', '0.5em'],
+				border: 'none',
+				width: '100%'
+			},
+			code: {
+				borderRadius: '4px'
+			},
+			playground: {
+				padding: '0 0 10px',
+				border: 'none'
+			}
+		}
+	},
 	plugins: [
 		css({
 			preprocessor: 'less',
