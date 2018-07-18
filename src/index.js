71f11e1ac88bae717c2086d943eef487f464f8b3
Adam Rich <adam.rich@thecontrolgroup.com>
2018-07-17 18:34:38 -0700

    adding drop-down component

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ export { default as Button } from './elements/Button/Button';
 export { default as Input } from './elements/Input/Input';
 export { default as Text } from './elements/Text/Text';
 export { default as List } from './elements/List/List';
+export { default as DropDown } from './elements/DropDown/DropDown';
 
 export { getBreakpoint } from './util/helpers';
 export { isMobile } from './util/helpers';
