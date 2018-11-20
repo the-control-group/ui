d202c1937f614c64269b4a2b0e3778d2677dfd33
Jeff <jeffrey.bue@thecontrolgroup.com>
2018-11-19 17:50:06 -0800

    New Toggle Component.

---
+++ src/elements/Toggle/Toggle.js
@@ -0,0 +1,42 @@
+/**
+ * Toggle
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import Common from '../Common/Common';
+
+const Toggle = ({
+	checked,
+	value,
+	loading,
+	...other
+}) => {
+	const combinedClasses = classNames(
+		'ui-toggle',
+		other.classes,
+		checked && 'on',
+		loading && 'loading'
+	);
+
+	return (
+		<Common
+			{...other}
+			classes={combinedClasses}
+			tag="div"
+			data-checked={checked}
+			data-value={value}
+		/>
+	);
+};
+
+Toggle.propTypes = {
+	loading: PropTypes.bool,
+	/** loading state */
+	checked: PropTypes.bool.isRequired,
+	/** position state */
+};
+
+export default Toggle;
