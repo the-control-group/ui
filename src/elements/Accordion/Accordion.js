bda6de0e249fd64b0da9803c79f1a0e3881ea133
Hanna Letska <hanna.letska@thecontrolgroup.com>
2019-03-26 11:24:07 -0700

    update accordion slide animation

--- src/elements/Accordion/Accordion.js
+++ src/elements/Accordion/Accordion.js
@@ -31,37 +31,72 @@ class Accordion extends Component {
 		super(props);
 
 		this.accordionContent = React.createRef();
+		this.disableToggle = false;
 
 		this.state = {
-			showContent: this.props.defaultExpanded
+			showContent: this.props.defaultExpanded,
+			toggle: this.props.defaultExpanded ? 'hide' : 'show'
 		};
 
 		this.toggleAccordion = this.toggleAccordion.bind(this);
 	}
 
 	toggleAccordion() {
+		if(this.disableToggle) return;
+
 		if(!this.state.showContent) {
 			this.setState({
-				showContent: true
+				showContent: true,
+				toggle: 'hide'
 			}, () => {
 				window.requestAnimationFrame(() => {
+					this.disableToggle = true;
+
+					// Set fixed height (based on height of content) and set transition-duration for .ui-accordion-content-wrapper.
+					// We need fixed height for CSS Transition to work and to animate slide down of accordion.
 					this.accordionContent.current.parentNode.style.transitionDuration = `${this.accordionContent.current.clientHeight * 2}ms`;
 					this.accordionContent.current.parentNode.style.height = `${this.accordionContent.current.clientHeight}px`;
+
+					// Set auto height for .ui-accordion-content-wrapper after animation is complete.
+					// We can't keep fixed height here because accordion may have other accordions inside of it and height of content may change.
+					setTimeout(() => {
+						this.disableToggle = false;
+						this.accordionContent.current.parentNode.style.height = 'auto';
+					}, this.accordionContent.current.clientHeight * 2);
 				});
 			});
 		} else {
-			this.accordionContent.current.parentNode.style.height = 0;
+			this.setState({
+				toggle: 'show'
+			});
+
+			window.requestAnimationFrame(() => {
+				this.disableToggle = true;
+
+				// Set height back to fixed for .ui-accordion-content-wrapper so we can animate slide up.
+				this.accordionContent.current.parentNode.style.height = `${this.accordionContent.current.clientHeight}px`;
+
+				// Set height and transition-duration for slide up animation.
+				setTimeout(() => {
+					this.accordionContent.current.parentNode.style.height = 0;
+				}, 10); // Need tiny timeout here
+			});
+
+			// Remove content from DOM after animation is complete
 			setTimeout(() => {
-				this.setState({showContent: false});
+				this.setState({
+					showContent: false
+				});
+				this.disableToggle = false;
 			}, this.accordionContent.current.clientHeight * 2);
 		}
 	}
 
 	render() {
 		const { header, children, className, toggleTextShow, toggleTextHide, buttonStyleToggle } = this.props,
-			{ showContent } = this.state,
-			toggleText = showContent ? toggleTextShow : toggleTextHide,
-			toggleIconClass = showContent ? 'arrow arrow-up' : 'arrow arrow-down';
+			{ showContent, toggle } = this.state,
+			toggleText = toggle === 'show' ? toggleTextShow : toggleTextHide,
+			toggleIconClass = toggle === 'hide' ? 'arrow arrow-up' : 'arrow arrow-down';
 
 		return (
 			<div className={classNames('ui-accordion', className)}>
@@ -70,13 +105,13 @@ class Accordion extends Component {
 						<Flex>{header}</Flex>
 						<Flex right={!isMobile()} max="200">
 							{buttonStyleToggle &&
-								<Button className="ui-accordion-toggle button-toggle" onClick={this.toggleAccordion} mini>
+								<Button className="ui-accordion-toggle button-toggle" onClick={this.toggleAccordion} mini ref={this.accordionToggle}>
 									<span>{toggleText}</span>
 									<img className={toggleIconClass} src={require('../../images/caret-down-white.svg')} style={{height: 20, width: 20}} />
 								</Button>
 							}
 							{!buttonStyleToggle &&
-								<div className="ui-accordion-toggle" onClick={this.toggleAccordion}>
+								<div className="ui-accordion-toggle" onClick={this.toggleAccordion} ref={this.accordionToggle}>
 									<span>{toggleText}</span>
 									<img className={toggleIconClass} src={require('../../images/caret-down.svg')} style={{height: 20, width: 20}} />
 								</div>
