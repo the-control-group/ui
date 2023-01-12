ffff805fbc95fa1134972be83d7ab2a645cbcf82
Matt <matt.dizon@peopleconnect.us>
2023-01-12 16:02:05 -0600

    fixed eslint err

--- src/elements/ModalProvider/ModalProvider.js
+++ src/elements/ModalProvider/ModalProvider.js
@@ -28,7 +28,9 @@ class ModalOverlay extends Component {
 		}
 
 		if(prevProps.show && !this.props.show) {
-			this.overlay.current !== null && this.overlay.current.classList.remove('enter-done');
+			if(this.overlay.current !== null) {
+				this.overlay.current.classList.remove('enter-done');
+			} 
 			setTimeout(() => this.setState({overlayVisible: false}), 200);
 		}
 	}
